998b9193b425ad7cdad067b7dd822fdf21574f13
Gilles Phan (macbook) <phan.gilles@gmail.com>
2025-11-11 01:40:49 +0100

    - create vehicle service

---
+++ src/app/shared/service/vehicle.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Vehicle } from '@app/core/models';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VehicleService {
+
+}
